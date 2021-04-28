77c64f18a8a6ec22823c8ec92f4bc8c7fffc3d77
vanemcb <vanem2201@gmail.com>
2021-04-28 09:12:08 -0500

    For loop fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range (122, 96, -1)
+for letter in range(122, 96, -1):
     if letter % 2 != 0:
         letter = letter - 32
-    print("{}".format(letter), end="")
+    print("{}".format(chr(letter)), end="")
