7c2cb6396ab2c654514238c28eacc0ef90dbf39f
vanemcb <vanem2201@gmail.com>
2021-04-27 23:51:52 -0500

    Function finihed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = 0
-    lower_case = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,\
-         110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+    lower_case = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+                  109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+                  121, 122]
     for letter in str:
         if ord(letter) in lower_case:
-            str[lower_case.index()] = chr()
-			
+            letter = chr(lower_case[lower_case.index(ord(letter))] - 32)
+        print("{}".format(letter), end="")
+    print("{}".format(""))
