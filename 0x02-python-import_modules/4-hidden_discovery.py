91c76f7f1ba44991a90e6d41529d6142a833f206
vanemcb <vanem2201@gmail.com>
2021-04-28 17:31:39 -0500

    Print condition added

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     import hidden_4
     file = dir(hidden_4)
     file.sort()
-    for i in range(8, 11):
-        print(file[i])
+    for name in file:
+        if name[0] != '_' and name[1] != '_':
+            print(file[i])
