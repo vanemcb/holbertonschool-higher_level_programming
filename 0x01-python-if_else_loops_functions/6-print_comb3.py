3d15e794ab089392d5b16c5281bf70f21b6e1fdf
vanemcb <vanem2201@gmail.com>
2021-04-28 10:12:59 -0500

    Program fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-for n in range(0, 10):
-    for n2 in range(1, 10):
-        print("{}{}".format(n, n2), end=", ")
+for n in range(0, 9):
+    for n2 in range(0, 10):
+        if n < n2 and n != 8:
+            print("{}{}".format(n, n2), end=", ")
+print("{}{}".format(n, n2))
