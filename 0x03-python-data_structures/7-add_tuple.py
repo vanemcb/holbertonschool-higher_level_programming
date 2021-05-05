f8b993244abfc15288a7b4f1d33f96770c890f08
vanemcb <vanem2201@gmail.com>
2021-05-05 18:53:14 -0500

    Function modified

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,7 +9,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         number1 = tuple_a[0]
         number2 = tuple_a[1]
     elif len_a == 0 and len_b == 0:
-        return tuple_c = ()
+        tuple_c = ()
+        return tuple_c
     elif len_a < 2:
         number1 = tuple_a[0] + tuple_b[0]
         number2 = tuple_a[0] + tuple_b[1]
