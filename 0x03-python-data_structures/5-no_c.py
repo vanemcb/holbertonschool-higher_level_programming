e1bc766a2cda5d3f1ebda697e416c2f24e03785b
vanemcb <vanem2201@gmail.com>
2021-05-05 16:36:32 -0500

    Function no_c modified

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    my_string2 = my_string
+    my_string2 = ""
     for char in my_string:
         if char == 'c' or char == 'C':
-            my_string2 = my_string[:i] + my_string[i + 1:]
-        i += 1
+            continue
+        my_string2 += char
     return my_string2
