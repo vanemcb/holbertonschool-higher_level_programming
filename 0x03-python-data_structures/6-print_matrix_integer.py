8fbcb96541f2371e4fc28b929cb9337cea941d0d
vanemcb <vanem2201@gmail.com>
2021-05-05 17:47:11 -0500

    Function modified

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
     for i in matrix:
         for x in range(0, len(i)):
             if x < len(i) - 1:
