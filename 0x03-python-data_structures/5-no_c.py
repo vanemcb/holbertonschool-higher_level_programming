e9c22789f101e21fa01f1a9e206306b5fcbee5ba
vanemcb <vanem2201@gmail.com>
2021-05-05 16:14:02 -0500

    Function modified

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     i = 0
-    my_string2 = my_string
+    my_string2 = ""
     for char in my_string:
         if char == 'c' or char == 'C':
             my_string2 = my_string[:i] + my_string[i + 1:]
