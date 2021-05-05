0c6fe085d6f9ac727ecc097d0660c099aa4e5d6b
vanemcb <vanem2201@gmail.com>
2021-05-05 18:48:47 -0500

    Function add_tuple created

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    if len_a == 0:
+        number1 = tuple_b[0]
+        number2 = tuple_b[1]
+    elif len_b == 0:
+        number1 = tuple_a[0]
+        number2 = tuple_a[1]
+    elif len_a < 2:
+        number1 = tuple_a[0] + tuple_b[0]
+        number2 = tuple_a[0] + tuple_b[1]
+    elif len_b < 2:
+        number1 = tuple_a[0] + tuple_b[0]
+        number2 = tuple_a[1] + tuple_b[0]
+    else:
+        number1 = tuple_a[0] + tuple_b[0]
+        number2 = tuple_a[1] + tuple_b[1]
+
+    tuple_c = (number1, number2)
+    return tuple_c
