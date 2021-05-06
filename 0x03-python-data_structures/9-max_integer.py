1423623f20fbfbf9e9f73ee3d6852a73013ff458
vanemcb <vanem2201@gmail.com>
2021-05-05 19:26:51 -0500

    Function max_integer created

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        return None
+
+    num = 0
+    for i in range(0, len(my_list)):
+        if my_list[i] > num:
+            num = my_list[i]
+    return num
