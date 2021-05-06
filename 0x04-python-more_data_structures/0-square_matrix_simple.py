2c60ac8bc6be971546d3cbabaee3dedf78b11c77
vanemcb <vanem2201@gmail.com>
2021-05-06 13:57:36 -0500

    Loop for fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_square = [[num[i]**2 for num in matrix] for i in range(len(matrix))]
+    matrix_square = [[x**2 for x in i] for i in matrix]
+#    for i in matrix:
+#        for x in i:
+#            x = x**2
+#            print(x)
     return matrix_square
