a5a764cc3adb3641229ff9988461ecb6deb9fea4
vanemcb <vanem2201@gmail.com>
2021-05-05 19:29:23 -0500

    Function max_integer created

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,9 +4,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    if my_list:
-        num = 0
-        for i in range(0, len(my_list)):
-            if my_list[i] > num:
-                num = my_list[i]
-        return num
+    num = 0
+    for i in range(0, len(my_list)):
+        if my_list[i] > num:
+            num = my_list[i]
+    return num
