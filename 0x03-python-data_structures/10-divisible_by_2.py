81e827bafc89332e146cf5813ec29e5814e95830
vanemcb <vanem2201@gmail.com>
2021-05-05 19:43:20 -0500

    Function fixed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,10 @@
 def divisible_by_2(my_list=[]):
 
     my_list2 = []
-    boolean = False
     for num in my_list:
         if num % 2 == 0:
             boolean = True
+        else:
+            boolean = False
         my_list2.append(boolean)
+    return my_list2
