1f7ebf5de3d2cc22104472b715f762abaf56a4f2
vanemcb <vanem2201@gmail.com>
2021-05-06 14:14:37 -0500

    Loop condition modified

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    i = 0
+
     my_list2 = my_list[:]
-    for num in my_list:
+    for i, num in enumerate(my_list):
         if num == search:
             my_list2[i] = replace
-        i += 1
+
     return my_list2
