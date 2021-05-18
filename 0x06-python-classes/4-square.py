a13ea89de6c13e023507516955aece0b71c9cec7
vanemcb <vanem2201@gmail.com>
2021-05-18 16:24:49 -0500

    Class that defines a square and access and update private attribute created

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Class that defines a square.
+"""
+
+
+class Square:
+    """ Class that defines a square and access and update
+    private attribute"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
