9d8c24632023d383e6cfabb2a86e7fc737e05ac9
vanemcb <vanem2201@gmail.com>
2021-05-18 15:48:20 -0500

    Comments fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Class that defines a square.
+"""
+
+
 class Square:
-    """ Class that defines a square."""
     def __init__(self, size):
         """ Initialization method.
 
