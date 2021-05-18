50d703b29c67ddf3e880fcf537c0553954764113
vanemcb <vanem2201@gmail.com>
2021-05-18 15:46:20 -0500

    Class Square with size created

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """ Class that defines a square."""
+    def __init__(self, size):
+        """ Initialization method.
+
+        Args:
+            size (int): square size.
+
+        """
+        self.__size = size
