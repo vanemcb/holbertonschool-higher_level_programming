3320476c5584363736230e93452c33b7265fb9b4
vanemcb <vanem2201@gmail.com>
2021-05-18 16:34:28 -0500

    Class that defines a square and prints it

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Class that defines a square.
+"""
+
+
+class Square:
+    """Class that defines a square and prints a square"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        for x in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
