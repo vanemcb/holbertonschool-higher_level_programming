da115bce9ec83129d8385dcf83a73ef4b0b78fe8
vanemcb <vanem2201@gmail.com>
2021-05-20 18:27:58 -0500

    Class Rectangle with __str__ method

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """Class square"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            peri = 0
+        else:
+            peri = (self.__height * 2) + (self.__width * 2)
+        return peri
+
+    def __str__(self):
+        display = []
+        for row in range(self.__height):
+            row_display = ""
+            for column in range(self.__width):
+                row_display += "#"
+            display.append(row_display)
+        display_rectangle = "\n".join(display)
+        return display_rectangle
