aeb8f762dc5aaedfec2dab2df451ccd43c6764e9
vanemcb <vanem2201@gmail.com>
2021-05-20 22:22:44 -0500

    Private attributes modified

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Object Ractangle initialization """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
