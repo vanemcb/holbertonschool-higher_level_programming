68720f008b90499de65a9d37b8587835fa68e84b
vanemcb <vanem2201@gmail.com>
2021-05-26 09:12:55 -0500

    String error fixed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
     if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("Size must be >= 0")
+        raise ValueError("size must be >= 0")
 
     for row in range(size):
         for column in range(size):
