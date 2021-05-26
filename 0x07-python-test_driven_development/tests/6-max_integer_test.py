a981c6253f54b95bf9a0fa50cf44c89530e9b7e2
vanemcb <vanem2201@gmail.com>
2021-05-25 23:13:30 -0500

    File modified

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Module to test the function max_integer
 """
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def list_empty(self):
+        self.assertIsNone(max_integer([]))
