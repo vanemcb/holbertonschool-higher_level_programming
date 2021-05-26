c38885677acb0b8c117213e96bde095a856f4efa
vanemcb <vanem2201@gmail.com>
2021-05-26 13:35:54 -0500

    File updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,4 +7,8 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def list_empty(self):
-        self.assertIsNone(max_integer([]))
+        self.assertEqual(max_integer([]), None)
+
+    def max_end(self):
+        list_num = [4, 8, 20]
+        self.assertEqual(max_integer(list_num), 20)
