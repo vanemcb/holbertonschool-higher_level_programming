d6511931e11c51012c4a20743a9fdaa19947e34b
vanemcb <vanem2201@gmail.com>
2021-06-01 18:56:48 -0500

    Clas BaseGeometry updated

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to create BaseGeometry class"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+    def area(self):
+        """Function that raises an exception"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Function that validates a value"""
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
