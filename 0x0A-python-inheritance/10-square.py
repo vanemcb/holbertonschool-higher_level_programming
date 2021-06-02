a62c89dbb669e4af3b21e7c4fc2295c33ab05826
vanemcb <vanem2201@gmail.com>
2021-06-01 19:17:58 -0500

    Class Rectangle created

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module to create Square class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size):
+        """Function to initialize an object"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
