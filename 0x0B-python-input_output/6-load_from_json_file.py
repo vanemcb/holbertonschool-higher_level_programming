ae5faf1939253963ea9b5371f3b709f96d90dc8f
vanemcb <vanem2201@gmail.com>
2021-06-03 11:08:50 -0500

    Function load_from_json_file created

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to create function load_from_json_file """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ Function that creates an Object from a “JSON file” """
+    with open(filename, "r", encoding="utf-8") as file:
+        json.load(file)
