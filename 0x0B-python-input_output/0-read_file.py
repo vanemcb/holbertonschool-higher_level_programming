f1e752336dfa6ad97b4ac4e6d084270798f784ee
vanemcb <vanem2201@gmail.com>
2021-06-02 21:11:56 -0500

    pep8 fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Module to create function read_file"""
+
+
 def read_file(filename=""):
+    """ Function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
