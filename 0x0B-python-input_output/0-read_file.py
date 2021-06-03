9e57b99a0b89f09d7afe6b11323280a6e99b8c9c
vanemcb <vanem2201@gmail.com>
2021-06-02 21:16:34 -0500

    Function read_file fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ Function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
