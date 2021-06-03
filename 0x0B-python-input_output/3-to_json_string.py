e34c78404c068c799b497a312c00bdbd068f74fc
vanemcb <vanem2201@gmail.com>
2021-06-03 10:00:32 -0500

    Function to_json_string created

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to create function to_json_string """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object (string) """
+
+    return json.dumps(my_obj)
