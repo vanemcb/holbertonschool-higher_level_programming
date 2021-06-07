49a9631677bcba0a739da1d8ad325db88edb175f
vanemcb <vanem2201@gmail.com>
2021-06-06 21:40:23 -0500

    Class square updated adding the public method update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,3 +28,26 @@ class Square(Rectangle):
         else:
             self.width = value
             self.height = value
+
+    def update(self, *args, **kwargs):
+
+        if len(args) != 0:
+            for index, arg in enumerate(args):
+                if index == 0:
+                    self.id = arg
+                if index == 1:
+                    self.width = arg
+                if index == 2:
+                    self.x = arg
+                if index == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
