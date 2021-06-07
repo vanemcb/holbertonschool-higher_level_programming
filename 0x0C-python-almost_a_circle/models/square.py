7f3e6fc000d882502f98ce1fd34b3138a7103f81
vanemcb <vanem2201@gmail.com>
2021-06-06 22:02:49 -0500

    Class Square updated adding the public method to_dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -51,3 +51,8 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        my_dict = {
+            "id": self.id, "size": self.width, "x": self.x, "y": self.y}
+        return my_dict
