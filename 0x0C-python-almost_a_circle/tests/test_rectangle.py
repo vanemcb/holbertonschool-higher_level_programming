64c82be3149d51ac42b7f8b1ba765e28b947af6a
vanemcb <vanem2201@gmail.com>
2021-06-08 17:57:06 -0500

    tests files modified

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,19 +1,26 @@
 """ Module to test Class Rectangle """
 import unittest
 
+import models.rectangle
 from models.rectangle import Rectangle
 
 
 class TestClassRectangle(unittest.TestCase):
 
     def test_docstring_module(self):
-        pass
+        comments_mod = models.rectangle.__doc__
+        self.assertTrue(len(comments_mod.splitlines()) > 0)
 
     def test_docstring_class(self):
-        pass
+        comments_class = Rectangle.__doc__
+        self.assertTrue(len(comments_class.splitlines()) > 0)
 
     def test_shebang(self):
-        pass
+        with open("models/rectangle.py", "r") as file:
+            first_line = file.readline()
+        self.assertEqual(first_line, '#!/usr/bin/python3\n')
 
     def test_pep8_style(self):
-        pass
+        import os
+        with os.popen("pep8 models/base.py") as mod_file:
+            self.assertEqual(mod_file.read(), "")
