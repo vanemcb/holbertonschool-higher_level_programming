39f29ebbb3adf810525f1da4511223ebefd24308
vanemcb <vanem2201@gmail.com>
2021-07-13 17:02:25 -0500

    New script created

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Script that lists all the cities of
 -- California that can be found in the database
-SELECT id, name from cities where state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id;
+SELECT id, name from cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY cities.id;
