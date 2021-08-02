e2c6227a46f8d276dbf92e674b91bb62ae1a4d45
vanemcb <vanem2201@gmail.com>
2021-08-02 15:40:58 -0500

    Script modified

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-let varString1;
-let varString2;
-if (typeof process.argv[2] === 'undefined') {
-  varString1 = 'undefined';
-} else {
-  varString1 = process.argv[2];
-}
-if (typeof process.argv[3] === 'undefined') {
-  varString2 = 'undefined';
-} else {
-  varString2 = process.argv[3];
-}
-console.log(varString1, 'is', varString2);
+console.log(process.argv[2], 'is', process.argv[3]);
