47e1652a3f4103cadd68d272d6d1591d50516d39
vanemcb <vanem2201@gmail.com>
2021-08-03 12:20:09 -0500

    Exportation modified

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 /* Class Square that defines a square and inherits from Rectangle */
-const Rectangle = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends require('./5-square') {
   constructor (size) {
     super(size, size);
     this.size = size;
