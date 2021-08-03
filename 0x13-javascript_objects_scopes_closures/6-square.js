97a9b8a4fb8acb8b751948da035e1796b64ea76b
vanemcb <vanem2201@gmail.com>
2021-08-03 11:42:15 -0500

    Instance method charPrint(c) created

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/* Class Square that defines a square and inherits from Rectangle */
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
