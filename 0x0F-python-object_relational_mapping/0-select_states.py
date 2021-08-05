3c50b0f847d63335dd85795b83000cb83815cfd1
vanemcb <vanem2201@gmail.com>
2021-08-05 16:14:46 -0500

    Script created

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3307,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3]
+)
+
+cursor = db.cursor()
+
+sql = "SELECT * FROM states ORDER BY id"
+
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for element in results:
+        print(element)
+except:
+    print("Error!!")
+
+
+db.close()
