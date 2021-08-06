3bdccc8f74784900a07abf7dd6877b919fe3a1f1
vanemcb <vanem2201@gmail.com>
2021-08-06 11:43:43 -0500

    New project created

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    # Open database connection
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # Cursor object
+    cursor = db.cursor()
+
+    # SQL query
+    n = (argv[4],)
+    sql = "SELECT * FROM states WHERE name = %s ORDER BY id", n
+
+    # execute SQL query
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
+
+    # Fetch all the rows in a list of lists
+    results = cursor.fetchall()
+
+    # Print elements
+    for element in results:
+        print(element)
+
+    cursor.close()
+
+    # Disconnect from server
+    db.close()
