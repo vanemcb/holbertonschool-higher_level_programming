6b97557ada04b2359a42a9254550880b7a6c6312
vanemcb <vanem2201@gmail.com>
2021-08-08 09:42:40 -0500

    Comments fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+""" Script that lists all states from the database hbtn_0e_0_usa
+"""
 
 from sys import argv
 import MySQLdb
