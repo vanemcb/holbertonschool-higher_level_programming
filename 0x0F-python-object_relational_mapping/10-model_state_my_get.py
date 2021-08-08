5b474d7bb01c7dca0d4536f1f4fe2822dc6d4207
vanemcb <vanem2201@gmail.com>
2021-08-08 11:13:45 -0500

    Comments created

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
+""" script that prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
 """
 
 from sys import argv
