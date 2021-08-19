a5caa7bb6481c36e0436a6586320c4256882f3f6
vanemcb <vanem2201@gmail.com>
2021-08-19 16:15:34 -0500

    Double quotes added to

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
-curl -s -I $1 | grep -i Content-Length | cut -d " "
+curl -s -I "$1" | grep -i Content-Length | cut -d " "
