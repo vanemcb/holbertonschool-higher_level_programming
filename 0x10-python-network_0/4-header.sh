4549516bda15320e3f6a46fce9795c8d6fae897b
vanemcb <vanem2201@gmail.com>
2021-08-19 17:33:48 -0500

    Script created

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -d "X-HolbertonSchool-User-Id: 98" $1
+curl -s -d "X-HolbertonSchool-User-Id: 98" -X GET $1
