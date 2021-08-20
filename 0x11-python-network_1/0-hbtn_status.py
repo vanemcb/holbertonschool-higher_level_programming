bee14b67724e50fcc8e38b728eb7a085594a666b
vanemcb <vanem2201@gmail.com>
2021-08-20 13:55:48 -0500

    pep8 syntax

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     print(
         "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
-        format(type(html), html,html.decode('utf-8')))
+        format(type(html), html, html.decode('utf-8')))
