5f4ed78502727457a69b2b386b483e1ded296153
vanemcb <vanem2201@gmail.com>
2021-08-20 15:47:02 -0500

    Script created

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script that takes in a letter and sends a POST request to
+http: // 0.0.0.0: 5000/search_user with the letter as a parameter. """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'http://bcd8ad1df972.c3bcb5f8.hbtn-cod.io:5000/search_user'
+
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+    r = requests.post(url, data={'q': q})
+
+    dict = r.json()
+
+    if dict == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(dict.get('id'), dict.get('name')))
