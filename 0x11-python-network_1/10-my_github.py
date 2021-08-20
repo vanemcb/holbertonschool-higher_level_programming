75c35108e154e633844b6a9017013e555e3a3a38
vanemcb <vanem2201@gmail.com>
2021-08-20 16:14:54 -0500

    Script created

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id """
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user',
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+
+    print(r.json().get('id'))
