1801bc21d9524fa89dcc95409aad03bb753a3d60
vanemcb <vanem2201@gmail.com>
2021-08-20 14:39:16 -0500

    Script created

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Script that fetches https://intranet.hbtn.io/status """
+
+import requests
+
+if __name__ == "__main__":
+
+    r = requests.get('https://intranet.hbtn.io/status')
+
+    print(
+        "Body response:\n\t- type: {}\n\t- content: {}".format(
+            type(r.text), r.text))
