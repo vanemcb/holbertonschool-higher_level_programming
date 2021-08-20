a8f3bf99340b16f42830c95ec57cf20083269b20
vanemcb <vanem2201@gmail.com>
2021-08-20 12:10:33 -0500

    utf8 check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,12 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        utf8_content = r.headers['content-type']
+        if 'utf-8' in utf8_content:
+            utf8_content = 'OK'
 
     print(
-        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: OK".
-        format(type(html), html))
+        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+        format(type(html), html, utf8_content))
