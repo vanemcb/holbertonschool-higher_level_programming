6caee423dfbc5a11e70248547ffce6e9cb666107
vanemcb <vanem2201@gmail.com>
2021-08-20 13:53:26 -0500

    utf8 decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Script that fetches https://intranet.hbtn.io/status """
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
-        utf8_content = r.headers['content-type']
-        if 'utf-8' in utf8_content:
-            utf8_content = 'OK'
+
+    url = 'https://intranet.hbtn.io/status'
+
+    with request.urlopen(url) as response:
+        html = response.read()
 
     print(
         "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
-        format(type(html), html, utf8_content))
+        format(type(html), html,html.decode('utf-8')))
