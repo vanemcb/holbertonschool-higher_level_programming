de0507f9987f4d1fa8bf146de1a71ac4c7cbf242
vanemcb <vanem2201@gmail.com>
2021-08-20 15:52:58 -0500

    url fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,15 +6,17 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = 'http://bcd8ad1df972.c3bcb5f8.hbtn-cod.io:5000/search_user'
 
+    url = 'http://0.0.0.0:5000/search_user'
     if len(argv) == 1:
         q = ""
     else:
         q = argv[1]
+
     r = requests.post(url, data={'q': q})
 
     dict = r.json()
+
     try:
         if dict == {}:
             print("No result")
