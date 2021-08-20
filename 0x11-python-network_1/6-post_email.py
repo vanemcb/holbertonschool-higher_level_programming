c18cdf7d78e8b1787b3c7dcae733e7971c7b3d2e
vanemcb <vanem2201@gmail.com>
2021-08-20 15:21:55 -0500

    condition added

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ of the response  """
 import requests
 from sys import argv
 
-r = requests.post(argv[1], data={'email': argv[2]})
-
-print(r.text)
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
