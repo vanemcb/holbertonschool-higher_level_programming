660911790fcfbb7f6a0b8e1275a8e12e539c72c3
vanemcb <vanem2201@gmail.com>
2021-08-24 13:45:31 -0500

    Script modified

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+
+    r = requests.get(url)
+
+    for i in range(10):
+        print("{}: {}".format(r.json()[i].get('sha'), r.json()[
+              i].get('commit').get('author').get('name')))
